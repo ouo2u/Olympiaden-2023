bdecaf77e0c4fbf4b97244a97ed487dd4693df39
Rittza <abod98451@gmail.com>
2023-11-24 08:59:53 +0100

    Klar med första uppgiften

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+Console.WriteLine("ordet ?");
+string ordet= Console.ReadLine();
+
+
+
+Console.WriteLine("Antal upprepningar ?");
+int upprepningar=int.Parse(Console.ReadLine());
+
+
+
+Console.WriteLine($"Svar: {String.Concat(Enumerable.Repeat(ordet, upprepningar))}");
+
